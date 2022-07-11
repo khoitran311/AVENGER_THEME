144136a617415d586784cc4e8818c6cfdac2990b
Khoi Tran 311 <khoi.tran@techupcorp.com>
2022-07-11 15:55:53 +0700

    THEME

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React from "react";
+import LinkItem from "./components/LinkItem";
+import ExampleLinkStarIcons from "./components/LinkStarIcons";
+import exampleThemeConfigs from "./data/theme_configs.data";
+import exampleUserData from "./data/user.data";
+
+export const App = ({
+    user: userData = exampleUserData,
+    themeConfigs = exampleThemeConfigs,
+    icons: LinkStarIcons = ExampleLinkStarIcons,
+    onClickLink
+}) => {
+    return (
+        <React.Fragment>
+            {userData && LinkStarIcons && (
+                <div className="linkstar">
+                    <div className="relative w-full h-full group">
+                        <div className="absolute w-full h-full">
+                            <img
+                                src={
+                                    themeConfigs?.cover ||
+                                    userData.cover_img_absolute ||
+                                    userData.avatar_img_absolute ||
+                                    themeConfigs?.default_avatar
+                                }
+                                className="object-cover object-center  w-full h-full"
+                                alt={userData.username}
+                            />
+                        </div>
+                        <div className="relative grid min-h-full backdrop-blur-2xl bg-cover bg-gradient-to-r from-pink-500 to-blue-500">
+                            <div className="relative z-10 flex flex-col h-full text-stone-700">
+                                <div>
+                                    {userData.cover_img_absolute ? (
+                                        <div className="w-full aspect-[5/2] heart sm:aspect-[4] xl:aspect-[5/1] px-3 pt-3 mx-auto sm:px-6 sm:pt-6 lg:px-8 lg:pt-8 xl:px-36 xl:py-0">
+                                            <img
+                                                src={userData.cover_img_absolute}
+                                                alt={`${userData.username}'s cover`}
+                                                className="object-cover object-center heart w-full h-full bg-gray-100 shadow-md rounded-2xl shadow-black/10 xl:rounded-t-none"
+                                            />
+                                        </div>
+                                    ) : (
+                                        <div className="h-24 md:h-32"></div>
+                                    )}
+                                </div>
+                                <div className="w-full max-w-screen-sm mx-auto mb-6 -mt-14 md:-mt-[3.75rem]">
+                                    <div className="mt-3 sm:mt-1 rounded-3xl z-50 md:mt-0 relative">
+                                        <div
+                                            className={`w-20 h-20 sm:w-24  md:w-28 sm:h-24 md:h-28 mx-auto overflow-hidden border-4 shadow-md   rounded-full bg-white ${
+                                                !userData.avatar_img_absolute && "p-4"
+                                            }`}
+                                        >
+                                            <img
+                                                src={userData.avatar_img_absolute || themeConfigs.default_avatar}
+                                                className="object-cover object-center w-full h-full"
+                                                alt={userData.username}
+                                            />
+                                        </div>
+                                        {/* <div className="w-44 h-44 rounded-full  absolute z-10 top-[-32px] left-[36.5%]">
+                                            <div className="w-full h-full rounded-full  border-[18px] border-dashed border-white"></div>
+                                        </div> */}
+
+                                        <div className="mt-10 font-semibold text-center text-white  md:text-lg">
+                                            <div className="drop-shadow-md">
+                                                {userData.display_name ||
+                                                    userData.last_name + " " + userData.first_name}
+                                            </div>
+                                            <div className="mt-1 drop-shadow-md">@{userData.username}</div>
+                                        </div>
+                                    </div>
+                                    <div className="flex-1 px-4 mt-10  md:mt-16 md:px-0">
+                                        {userData.user_links.map(link => (
+                                            <LinkItem
+                                                key={link.uuid}
+                                                link={link}
+                                                icons={LinkStarIcons}
+                                                onClickLink={onClickLink}
+                                            />
+                                        ))}
+                                    </div>
+                                    <div className="w-full h-72 mt-10 lg:hidden md:hidden sm:block">
+                                        <img
+                                            className="w-full h-full"
+                                            src="https://i.pinimg.com/originals/0c/57/dd/0c57dd57539635c63467bbcdbf0611ed.gif"
+                                            alt=""
+                                        ></img>
+                                    </div>
+                                </div>
+                                <div className="mx-auto mt-auto">
+                                    <div className="flex flex-col items-center text-white w-full pb-8 mx-auto mt-10">
+                                        <div className="mb-1 text-sm font-semibold">Powered By</div>
+                                        <img src={themeConfigs?.linkstar_logo} alt="LinkStar" className="h-10" />
+                                        <div className="mt-1 text-sm font-semibold">
+                                            © LinkStar. All rights reserved.
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </React.Fragment>
+    );
+};
---
+++ src/components/LinkItem.js
@@ -0,0 +1,46 @@
+import React from "react";
+import LinkItemIcon from "./LinkItemIcon";
+
+const LinkItem = ({ link, icons: LinkStarIcons, onClickLink }) => {
+    const { title, link: url, host, is_published, host_key } = link;
+
+    const iconColors = {
+        facebook: "text-blue-600 border-blue-600",
+        youtube: "text-red-600 border-red-600",
+        twitter: "text-blue-500 border-blue-500",
+        instagram: "text-red-600 border-red-600",
+        linkedin: "text-blue-600 border-blue-600",
+        soundcloud: "text-red-600 border-red-600",
+        telegram: "text-blue-600 border-blue-600",
+        default: "text-zinc-800 border-zinc-800"
+    };
+
+    const handleClick = e => {
+        e.preventDefault();
+        onClickLink(link.uuid);
+    };
+
+    return (
+        <React.Fragment>
+            <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className={` flex items-center group rounded-2xl bg-white py-2 px-3 md:py-3 mb-4 md:px-4 last:mb-0 transition-all border-2    hover:bg-gradient-to-r  from-blue-500 to-pink-500  outline-none hover:scale-105 text-black ${
+                    !is_published && "opacity-60"
+                } shadow-md`}
+                onClick={handleClick}
+            >
+                <LinkItemIcon isAlwaysShow className={`${iconColors[host_key] || iconColors.default} opacity-70`}>
+                    <LinkStarIcons hostname={host_key} size={24} />
+                </LinkItemIcon>
+                <div className="flex-1 px-4 font-semibold text-center">{title || host}</div>
+                <LinkItemIcon invisible isAlwaysShow>
+                    <LinkStarIcons hostname={host_key} size={24} />
+                </LinkItemIcon>
+            </a>
+        </React.Fragment>
+    );
+};
+
+export default LinkItem;
